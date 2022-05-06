13c1aeffc2dd5e52651f7d51d7935a22d289fcb0
rshaikhm <rshaikhm@gmail.com>
2022-05-06 17:31:42 +0400

    initial commit

---
+++ ClientNewSecureWebApi/ClientNewSecureWebApi/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+using System.Net.Http.Headers;
+using System.Linq;
+
+namespace ClientNewSecureWebApi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            RunAsync().GetAwaiter().GetResult();
+            
+        }
+
+        private static async Task RunAsync()
+        {
+            AuthConfig config = AuthConfig.ReadJsonFromFile("appsettings.json");
+            IConfidentialClientApplication app;
+            app = ConfidentialClientApplicationBuilder.Create(config.ClientId)
+                .WithClientSecret(config.ClientSecret)
+                .WithAuthority(new Uri(config.Authority))
+                .Build();
+
+            string[] ResourceIds = new string[] { config.ResourceId };
+            AuthenticationResult result = null;
+            try
+            {
+                result = await app.AcquireTokenForClient(ResourceIds).ExecuteAsync();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Token Acquired \n");
+                Console.WriteLine(result.AccessToken);
+                Console.ResetColor();
+            }
+            catch (MsalClientException ex)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(ex.Message);
+                Console.ResetColor();
+                //throw;
+            }
+
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                var httpClient = new HttpClient();
+                var defaultRequestHeader = httpClient.DefaultRequestHeaders;
+
+                if (defaultRequestHeader.Accept == null || !defaultRequestHeader.Accept.Any
+                    (m=>m.MediaType=="application/json"))
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new
+                        MediaTypeWithQualityHeaderValue("application/json"));
+                }
+
+                defaultRequestHeader.Authorization =
+                    new AuthenticationHeaderValue("bearer", result.AccessToken);
+
+                HttpResponseMessage response = await httpClient.GetAsync(config.BaseAddress);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string json = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(json);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Failed to call API: {response.StatusCode}");
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.Write($"Content: {content}");
+                }
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ ClientNewSecureWebApi/ClientNewSecureWebApi/obj/Release/netcoreapp3.1/PubTmp/Out/app_data/Jobs/Triggered/ClientNewSecureWebApi/run.cmd
@@ -0,0 +1,1 @@
+dotnet ClientNewSecureWebApi.dll %*
